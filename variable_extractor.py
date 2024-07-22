49487a5460010691593d497f7b2cb6c32aca8ca0
Andrew <andrew@TOSHIBA-U2187-OFFICELINK-NET46UHZ.local>
2024-07-22 12:19:13 -0500

    fixed get_document_path method

--- variable_extractor.py
+++ variable_extractor.py
@@ -46,21 +46,17 @@ class CaseDirectory:
         """
         Converts all documents missing a file extension in directory to text files
         """
-        self._convert_to_text(self.parent_directory)
+        def convert_directory(parent: str) -> None:
+            for f in os.listdir(parent):
+                path = os.path.join(parent, f)
+                if os.path.isfile(path):
+                    _, ext = os.path.splitext(path)
+                    if ext == "":
+                        os.rename(path, path+".txt")
+                if os.path.isdir(path):
+                    convert_directory(path)
+        convert_directory(self.parent_directory)
 
-    def _convert_to_text(self, parent) -> None:
-        """
-        Converts all documents missing a file extension in directory to text files
-        """
-        for f in os.listdir(parent):
-            path = os.path.join(parent, f)
-            if os.path.isfile(path):
-                _, ext = os.path.splitext(path)
-                if ext == "":
-                    os.rename(path, path+".txt")
-            if os.path.isdir(path):
-                self._convert_to_text(path)
-    
     def get_proportion_downloaded(self) -> float:
         """
         returns proportion of total documents downloaded
@@ -100,6 +96,9 @@ class CaseMetadata:
         return {field: self.get_info_field(field) for field in fields} | {"metadata_path": self.path}
 
     def _get_docs_json(self) -> list[dict]:
+        """
+        returns contents of docs.json file as json
+        """
         docs_path = self.path.replace("metadata", "docs")
         with open(docs_path) as f:
             return json.loads(f.read())
@@ -135,6 +134,9 @@ class CaseMetadata:
         """
         returns dataframe of docket_report entries with associated metadata
         Adds column for path to document (if downloaded)
+        Assumes document names are derived by removing the case link portion 
+        of the document link and documents are contained in a folder within
+        the same parent directory as metadata.json
         """
         def get_document_path(link_viewer):
             case_link = self.get_info_field("link")
@@ -143,9 +145,10 @@ class CaseMetadata:
                 file = file[:-1] if file[-1] == "/" else file
                 file += ".txt"
                 parent = self.path.replace("metadata.json", "")
-                path = os.path.join(parent, file)
-                if os.path.isfile(path):
-                    return path
+                paths = [os.path.join(f.path, file) for f in os.scandir(parent) if os.path.isdir(f)]
+                for path in paths:
+                    if os.path.isfile(path):
+                        return path
             return ""
         
         df = pd.json_normalize(self.metadata["docket_report"])
@@ -156,7 +159,7 @@ class CaseMetadata:
         if "entry_date" in df.columns:
             df.entry_date = pd.to_datetime(df.entry_date)
         if "contents" in df.columns:
-            df.contents = df.contents.apply(lambda html: BeautifulSoup(html).text)
+            df.contents = df.contents.apply(lambda html: BeautifulSoup(html, features="html.parser").text)
         if "link_viewer" in df.columns:
             df["document_path"] = df.link_viewer.apply(get_document_path)
         return df
@@ -170,7 +173,7 @@ class CaseMetadata:
         if "docket_report" in self.metadata:
             for docket in self.metadata["docket_report"]:
                 if "contents" in docket:
-                    entries.append(BeautifulSoup(docket["contents"]).text)
+                    entries.append(BeautifulSoup(docket["contents"], features="html.parser").text)
         return entries
     
     def get_documents(self) -> list[str]:
@@ -508,6 +511,64 @@ class BenchRulingClassifier(VariableExtractor):
             has_result_keywords = lambda s: any([word in s.lower() for word in result_keywords])
             return has_result_keywords(s) and has_party(s)
         
+
+        super().__init__(metadata_path,
+                         prompt,
+                         "category",
+                         "undetermined",
+                         keyword_filter,
+                         embedding_model,
+                         separators,
+                         chunk_size,
+                         overlap,
+                         language_model,
+                         llm_document_count,
+                         document_separator,
+                         llm_context_length)
+
+
+class TrialTypeClassifier(VariableExtractor):
+    def __init__(self,
+                 metadata_path: str,
+                 embedding_model: str = "mxbai-embed-large",
+                 separators: list[str] = ["\n\n", "\n", ". ", "!", "?", ".", ";", ":", ",", " ", ""],
+                 chunk_size: int = 700,
+                 overlap: int = 0,
+                 language_model: str = "mistral",
+                 llm_document_count: int = 9,
+                 document_separator: str = "||",
+                 llm_context_length: int = 2048) -> None:
+        """
+        Classifies trial type (bench trial or jury trial).
+        Response includes reasoning + category (bench trial, jury trial, undetermined)
+        Keyword filter requires documents contain outcome keyword
+        (verdict, ruling, judgement, etc.)
+
+        """
+        prompt = """
+        You are an expert legal analyst. The given documents relate to a legal case in the United States. All descriptions correspond to the same case. Your task is to classify the outcome of this case into one of the following categories:
+
+        jury trial
+        bench trial
+        undetermined
+
+        Jury trials include documents such as verdict sheets or juror instructions. Bench trials include a decision, a finding, or opinion by the judge. Bench trials do not include references to a jury.
+        Some cases are resolved by a plea, a settlement, an agreed judgement, or are dismissed before trial. These cases usually reference a plea agreement, dismissal, settlement, or judgement.
+        These cases will not include an opinion, a verdict, or sentencing. 
+        If any descriptions identifying the case as a jury trial are found, categorize the case outcome as "jury trial". 
+        If any descriptions identifying the case as a bench trial are found, categorize the case outcome as "bench trial". 
+        If the descriptions do not identify a trial occuring or the documents are ambiguous, categorize the case outcome as undetermined.
+
+
+        Respond with a JSON object in the format "{"reasoning": "...", "category": "..."}" 
+        If a description identifying the case outcome is found, reasoning should be a string in the form "The event _ during the case supports the case being _." 
+        Otherwise, the reasoning should be a string in the form, "The case includes _, which could be found in cases with multiple outcomes. Therefore, the case outcome is undetermined". Do not summarize the case or documents. 
+        category should only include one of the following categories as a string: jury trial, bench trial, undetermined.
+        """
+
+        def keyword_filter(s):
+            result_keywords = ["verdict", "judgement", "opinion", "decision", "decree", "order", "ruling", "disposition", "finding", "trial"]
+            return any([word in s.lower() for word in result_keywords])        
 
         super().__init__(metadata_path,
                          prompt,
